717e57d3a1ef1d19a29d55192a6700b40cf1b39e
Coding Otaku <contact@codingotaku.com>
2020-07-19 06:48:07 +0000

    inital commit

---
+++ data/blogs.js
@@ -0,0 +1,21 @@
+export const mainBlogs = [{
+      path:'/pages/welcome/',
+      title:'Welcome',
+      subtitle:'Welcome to exeami blogs!',
+      image:'/images/welcome.svg'
+    },
+    {
+      path:'/pages/why-should-i-switch-to-signal/',
+      title:'Why should I switch to Signal?',
+      subtitle:'A privacy respecting secure messenger.',
+      image:'/images/signal.jpg'
+    },
+    {
+      path:'/pages/why-should-i-switch-to-signal/',
+      title:'Homomorphic Encryption ',
+      subtitle:'Trying figure out what it is.',
+      image:'/images/homomorphic_encryption.jpg'
+    },
+  ];
+
+export const allBlogs = [];
---
+++ data/nav.js
@@ -0,0 +1,17 @@
+{
+  icon:'/icons/logo.svg',
+  links:[{
+    name:'ExeAmi',
+    url:'https://exeami.com',
+    isNewTab:true
+  },
+  {
+    name:'About',
+    url:'',
+  },
+  {
+    name:'Contact',
+    url:'',
+  }
+  ]
+}
