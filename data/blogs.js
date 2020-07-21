81f0d80b9a17dd09d04a4525ff89cb19f95ee1a3
Amitha Lingala <amitha.lingala@hp.com>
2020-07-21 21:34:10 +0530

    added food blog

--- data/blogs.js
+++ data/blogs.js
@@ -1,21 +1,29 @@
-export const mainBlogs = [//{
-    //   path:'/pages/welcome/',
-    //   title:'Welcome',
-    //   subtitle:'Welcome to exeami blogs!',
-    //   image:'/images/welcome.svg'
-    // },
-    {
-      path:'/pages/why-should-i-switch-to-signal/',
-      title:'Why should I switch to Signal?',
-      subtitle:'A privacy respecting secure messenger.',
-      image:'/images/signal/signal.jpg'
-    },
-    {
-      path:'/pages/homomorphic-encryption/',
-      title:'Homomorphic Encryption ',
-      subtitle:'Trying figure out what it is.',
-      image:'/images/HE/HE_1.png'
-    },
-  ];
-
-export const allBlogs = [];
+export const mainBlogs = [
+  {
+    type: 'Privacy',
+    blogs: [
+      {
+        path: '/pages/why-should-i-switch-to-signal/',
+        title: 'Why should I switch to Signal?',
+        subtitle: 'A privacy respecting secure messenger.',
+        image: '/images/signal/signal.jpg'
+      },
+      {
+        path: '/pages/homomorphic-encryption/',
+        title: 'Homomorphic Encryption ',
+        subtitle: 'Trying figure out what it is.',
+        image: '/images/HE/HE_1.png'
+      },
+    ]
+  }, {
+    type: 'Simple Recipes',
+    blogs: [
+      {
+        path: '/pages/tomato-chutney/',
+        title: 'Tomato chutney ',
+        subtitle: 'Must try...!',
+        image: '/images/recipes/th/final.png'
+      },
+    ]
+  }
+]
--- scripts/main.js
+++ scripts/main.js
@@ -1,29 +1,38 @@
-import {mainBlogs} from '/data/blogs.js'
+import { mainBlogs } from '/data/blogs.js'
 
 function generateBlogs() {
   const cardHolder = document.getElementById('card-holder');
-  let columns = createElement('div', ['columns']);
-  mainBlogs.forEach((blog, i) => {
-    console.log(i);
-    if (i% 4 === 0) {
-      cardHolder.appendChild(columns);
-      columns = createElement('div', ['columns']);
-    }
-    generateCard(blog, columns);
+  // Get all available blog types and create row for each
+  mainBlogs.forEach((blogType) => {
+    let columns = createElement('div', ['columns']);
+    // Create a card for each blog
+    blogType.blogs.forEach((blog, i) => {
+      if (i % 4 === 0) {
+        cardHolder.appendChild(columns);
+        columns = createElement('div', ['columns']);
+      }
+      generateCard(blog, columns);
+    });
+
+    // Add title for blog type
+    const type = createElement('div', ['title', 'has-text-dark']);
+    type.innerHTML = blogType.type; 
+    cardHolder.appendChild(type);
+    // Append all blogs
+    cardHolder.appendChild(columns);
   });
-  cardHolder.appendChild(columns);
 }
 
 function generateCard(blog, columns) {
-  const column = createElement('div', ['column','is-3']);
-  const card = createElement('a', ['card','box']);
+  const column = createElement('div', ['column', 'is-3']);
+  const card = createElement('a', ['card', 'box']);
   card.href = blog.path;
 
   const cardImage = createElement('div', ['card-image']);
-  const figure = createElement('div', ['image','is-2by1']);
-  const img = createElement('img',[]);
+  const figure = createElement('div', ['image', 'is-2by1']);
+  const img = createElement('img', []);
 
-  img.src =blog.image;
+  img.src = blog.image;
 
   figure.appendChild(img);
   cardImage.appendChild(figure);
@@ -37,7 +46,7 @@ function generateCard(blog, columns) {
   title.innerHTML = blog.title;
   subtitle.innerHTML = blog.subtitle;
   content.appendChild(title);
-  content.appendChild(createElement('br',[]));
+  content.appendChild(createElement('br', []));
   content.appendChild(subtitle);
 
   cardContent.appendChild(content);
