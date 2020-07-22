690f9e81971bde98da2ffed5d6bf58d31b49c2eb
Amitha Lingala <amitha.lingala@hp.com>
2020-07-22 10:51:39 +0530

    updated about page

--- data/blogs.js
+++ data/blogs.js
@@ -9,16 +9,16 @@ export const mainBlogs = [
         image: '/images/DP/dp.jpg'
       },
       {
-        path: '/pages/why-should-i-switch-to-signal/',
-        title: 'Why should I switch to Signal?',
-        subtitle: 'A privacy respecting secure messenger.',
-        image: '/images/signal/signal.jpg'
-      },
-      {
         path: '/pages/homomorphic-encryption/',
         title: 'Homomorphic Encryption ',
         subtitle: 'Trying figure out what it is.',
         image: '/images/HE/HE_1.png'
+      },
+      {
+        path: '/pages/why-should-i-switch-to-signal/',
+        title: 'Why should I switch to Signal?',
+        subtitle: 'A privacy respecting secure messenger.',
+        image: '/images/signal/signal.jpg'
       },
       
     ]
