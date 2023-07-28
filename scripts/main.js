6be1d894b1640e2566ba6d7a9be38ae7378f14cf
amithalingala <amithalingala@tutamail.com>
2023-07-28 19:48:40 +0100

    preload css

--- scripts/main.js
+++ scripts/main.js
@@ -6,13 +6,6 @@ function generateBlogs() {
 
   // Get all available blog types and create row for each
   mainBlogs.forEach((blogType) => {
-    // Add title for blog type
-    // const title = createElement("div", {
-    //   classes: ["title", "has-text-dark", "has-text-centered"],
-    // });
-    // title.innerHTML = blogType.type;
-    // cardHolder.appendChild(title);
-
     let columns = createElement("div", { classes: ["columns"] });
     // Create a card for each blog
     blogType.blogs.forEach((blog, i) => {
